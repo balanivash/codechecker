e7fa0123fb319238fd71b30396ec3a8261b92f7b
Aditya Manthramurthy <aditya87@gmail.com>
2010-04-19 00:26:38 +0530

    This is an old branch, which we will no longer use

---
+++ codechecker/backend/Config.py
@@ -0,0 +1,23 @@
+import ConfigParser
+import os
+
+class Config:
+    
+    def __init__(self):
+        self.config = ConfigParser.ConfigParser()
+        self.config.read("utils/codechecker.conf")
+        self.runpath = self.config.get("BackendMain", "RunsPath")
+        self.outputLimit = int(self.config.get("BackendMain","OutputFileSizeLimit"))
+        
+        # Check if setuid_helper exists.
+        self.shPath = self.config.get("BackendMain", "CheckerRoot") + "codechecker/backend/setuid_helper"
+        assert os.path.isfile(self.shPath) and os.access(self.shPath, os.X_OK)
+        
+
+if __name__ == "__main__":
+    
+    config = Config()
+    print config.config.sections()
+    print config.config.get("CompileCommands", "C_compile")
+    
+
