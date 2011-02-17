c9e086504526a439c0e9029cd2ad9f5fd62eb2c1
Aditya Manthramurthy <aditya.mmy@gmail.com>
2011-02-18 00:35:44 +0530

    Updated checker with some reorganisation and fixes (github Issue #3). More to come.

--- src/checker/cc_backend/Checker.py
+++ src/checker/cc_backend/Checker.py
@@ -12,6 +12,40 @@ from score.score import Score
 from Config import Config
 
 
+def wait_for_submission(store):
+    """
+    Returns a submission to be evaluated. Blocks execution until one
+    is available.
+    """
+    is_waiting = False
+    while True:
+        submission = store.get_submission()
+        if submission is not None:
+            yield submission
+        else:
+            if not is_waiting:
+                is_waiting = True
+                print "Waiting for a submission."
+            time.sleep(2)
+
+
+def compile_submission(store, submission):
+    """
+    Attempts to compile the submission. Updates the status in the
+    store. Return True or False.
+    """
+    compiler_res = compiler.compile_source(submission["src_file"])
+    if compiler_res["retcode"] == 0:
+        store.set_compile_status("COMPILED", err_msg=None,
+                                 sub_id=submission["id"])
+        return compiler_res
+    else:
+        store.set_compile_status("COMPILATION ERROR",
+                                 err_msg=status_info.err_msg,
+                                 sub_id=submission["id"])
+        return None
+
+
 def main():
 
     config = Config("conf/codechecker.conf")
@@ -20,45 +54,40 @@ def main():
     compiler = Compiler(config)
     score = Score()
 
-    is_waiting = True
-
-    while True:
-        # get a queued submission.
-        submission = store.get_submission()
-
-        # if no submission is found wait for one.
-        if submission is None:
-            if not is_waiting:
-                is_waiting = True
-                print "Waiting for a submission."
-            time.sleep(2)
-            continue
-
-        is_waiting = False
+    # Each iteration of the loop below evalutes a submission.
+    for submission in wait_for_submission(store):
 
         # compile the submission
-        compiler_res = compiler.compile_source(submission["src_file"])
-        if compiler_res["retcode"] == 0:
-            store.set_compile_status("COMPILED", err_msg=None,
-                                     sub_id=submission["id"])
-        else:
-            store.set_compile_status("COMPILATION ERROR",
-                                     err_msg=status_info.err_msg,
-                                     sub_id=submission["id"])
+        compiler_res = compile_submission(store, submission)
+        if compiler_res == None:
             continue
 
         # Evaluate the queued submission. Somewhere in the following
         # loop it is also possible that the program fails - need to
         # set the status to runtime error status.
         test_group_scores = []
-        for test_grp in store.get_test_group(submission["prob_id"]):
+        for testset in store.get_all_testsets(submission["prob_id"]):
+            testset_info = {}
+            testset_info["memlimit"] = testset["memlimit"]
+            testset_info["timelimit"] = testset["timelimit"]
+            testset_info["cust_execute"] = testset["cust_execute"]
+            testset_info["is_cust_scored"] = testset["is_cust_scored"]
+            testset_info["infile"] = []
+            testset_info["reffile"] = []
+            for testcase in store.get_all_testcases(testset["testset_id"]):
+                testset_info["infile"].append(testcase["infile"])
+                testset_info["reffile"].append(testcase["reffile"])
+
             result_set = evaluator.eval_submission(submission,
-                                                   test_grp, compiler_res["run_cmd"])
+                                                   testset_info,
+                                                   compiler_res["run_cmd"])
+            #TODO: implement the below functions properly.
             test_grp_score = score.score_group(prob_id,
                                                result_set)
             store.set_test_group_score(test_grp_score,
-                problem_id=prob_id, test_group_id=test_grp["id"],
-                submission_id=submission["id"])
+                                       problem_id=prob_id,
+                                       test_group_id=test_grp["id"],
+                                       submission_id=submission["id"])
             test_group_scores.append(test_grp_score)
 
         # compute and set the overall score.
